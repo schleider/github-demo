f91820269c5f118fef3583076dc229dc86fcc4ef
Yair Schleider <yair@y-tree.com>
2021-12-29 14:14:03 +0000

    added add func

--- calc.py
+++ calc.py
@@ -1,16 +1,18 @@
-
 def add(x, y):
-    pass
+    return x + y
 
 
 def subtract(x, y):
-    return x-y
+    return x - y
 
 
 def multiply(x, y):
     # this will return the multiplication of two numbers
-    return x*y
+    return x * y
 
 
 def divide(x, y):
-    return x/y
+    return x / y
+
+
+
